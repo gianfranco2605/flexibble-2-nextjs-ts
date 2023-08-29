a868d1340546e2aff8170bf3dbf3164cba7f7ab9
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2023-08-29 22:53:21 +0200

    env

--- lib/session.ts
+++ lib/session.ts
@@ -17,19 +17,22 @@ export const authOptions: NextAuthOptions = {
     ],
     jwt: {
         encode: ({ secret, token }) => {
-            const encodedToken = jsonwebtoken.sign({
-                ...token,
-                iss: 'grafbase',
-                exp: Math.floor(Date.now() / 1000) + 60 * 60
-            }, secret)
-            return encodedToken;
+          const encodedToken = jsonwebtoken.sign(
+            {
+              ...token,
+              iss: "grafbase",
+              exp: Math.floor(Date.now() / 1000) + 60 * 60,
+            },
+            secret
+          );
+          
+          return encodedToken;
         },
         decode: async ({ secret, token }) => {
-            const decodeToken = jsonwebtoken.verify(token!, secret) as JWT
-
-            return decodeToken;
-        }
-    },
+          const decodedToken = jsonwebtoken.verify(token!, secret);
+          return decodedToken as JWT;
+        },
+      },
     theme: {
         colorScheme: 'light',
         logo: '/logo.png'
